dab06687beb430b3afa19e19f2f6dbf9cea0681b
jff <22579497+jffin@users.noreply.github.com>
2020-09-05 04:22:44 +0300

    refactoring 0.7

--- script.py
+++ script.py
@@ -1,115 +1,79 @@
 import json
 import asyncio
 import argparse
-from typing import Dict, List
+from typing import Dict, List, NamedTuple
 
 from yarl import URL
 from colorama import Fore
+from attr import attrs, attrib, Factory
 
 import aiohttp
 from aiohttp import InvalidURL, ClientConnectorError
-
 
 SUCCESS_RESPONSE_CODE = 200
 JWT_SECRETS_FILE_NAME = 'jwt.secrets.list'
 JWT_SECRETS_FILE_URL: str = 'https://raw.githubusercontent.com/wallarm/jwt-secrets/master/jwt.secrets.list'
 
 
-class LeakedCookie:
-    def __init__(self):
-        self._url: URL = URL()
+class RunConfig(NamedTuple):
+    url: str
+    output: str = 'result.json'
+    quiet: bool = False
+    print_cookies: bool = False
+    no_color: bool = True
 
-        self._cookies: Dict[str, str] = {}
-        self._jwt_secrets: List[str] = []
-        self._result: List[Dict[str, str]] = []
 
-        self.quiet: bool = False
-        self.print_cookies_mod: bool = False
-        self._result_file: str = 'result.json'
-        self._print_default: bool = False
+class PrettyPrint:
 
-    @classmethod
-    async def run(cls):
+    def __init__(self, no_color: bool = False):
+        self.no_color: bool = no_color
+
+    def print_in_color(self, text: str, danger: bool = False) -> None:
         """
-        starts async jobs from the main script file
+        prints output in color or without
+        depends on argument `--no-color`
+        green for success results
+        red if jwt secret found
+        :param text: str text to print
+        :param danger: bool print in green or red color
         :return: None
         """
-        # creating class
-        obj = cls()
+        if self.no_color:
+            print(text)
+        else:
+            if danger:
+                print(f'{Fore.RED}{text}')
+            else:
+                print(f'{Fore.GREEN}{text}')
 
-        # preparation
-        obj.argument_parser()
-
-        # makes request and receiving cookies
-        await obj.make_request_to_target()
-
-        # print if needs found cookies from the target
-        if obj.print_cookies_mod:
-            obj.print_cookies()
-
-        # parsing cookies
-        await obj.parse_cookies()
-
-        # if not quiet print result to stdout
-        if not obj.quiet:
-            obj.print_result()
-
-        # saving results to a file
-        obj.save_result_to_file()
-
-    def argument_parser(self) -> None:
+    def print_result(self, result: List[Dict[str, str]]) -> None:
         """
-        Parsing arguments
+        prints result to stdout
+        :type result: List[Dict[str, str]]
         :return: None
         """
-        # Construct the argument parser
-        ap = argparse.ArgumentParser(description='Finds jwt secrets in cookies')
-        # Add the arguments to the parser
-        ap.add_argument('-u', '--url', required=True, help='url', type=str)
+        if result:
+            self.print_in_color(f'\n[!] Secrets in cookies found:', True)
+        else:
+            self.print_in_color(f'\n[+] Success, no jwt secrets found')
+        for value in result:
+            for key, cookie in value.items():
+                self.print_in_color(f'{key}: {cookie}', True)
 
-        ap.add_argument(
-            '-o', '--output', required=False, help='file to save result in json', default='result.json', type=str)
-
-        ap.add_argument(
-            '-q', '--quiet', required=False, help='quiet mod, only save to file', action='store_true', default=False)
-        ap.add_argument('-p', '--print-cookies', required=False, action='store_true', default=False,
-                        help='print to stdout found cookie list')
-        ap.add_argument('-n', '--no-color', required=False, action='store_true', default=False,
-                        help='print without colors to stdout')
-
-        # parse arg
-        args = ap.parse_args()
-
-        self.url = args.url
-
-        if args.quiet:
-            self.quiet = True
-        if args.print_cookies:
-            self.print_cookies_mod = True
-        if args.no_color:
-            self.print_default = True
-
-        if args.output:
-            self.result_file = args.output
-
-    @property
-    def url(self) -> URL:
+    def print_cookies(self, cookies: Dict) -> None:
         """
-        getter for a target url
-        :return: URL url
-        """
-        return self._url
-
-    @url.setter
-    def url(self, url: URL) -> None:
-        """
-        setting a target url
-        :param url: str
+        prints cookies to stdout
         :return: None
         """
-        self._url = URL(url)
+        self.print_in_color(f'\nReceived cookies:')
+        for key, value in cookies.items():
+            self.print_in_color(f'{key}: {value}')
 
-    async def make_request_to_target(self) -> None:
+
+class Requests:
+
+    @staticmethod
+    async def make_request_to_target(url: URL, printer: PrettyPrint) -> Dict:
         """
         do request to a target
         :return: None
@@ -117,15 +81,55 @@ class LeakedCookie:
         headers = {'User-agent': 'Googlebot-News', 'Cookie': 'security=low;'}
         async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as s:
             try:
-                async with s.get(self.url, headers=headers):
-                    self.cookies = s.cookie_jar.filter_cookies(self.url)
+                async with s.get(url, headers=headers):
+                    return s.cookie_jar.filter_cookies(url)
             except InvalidURL:
-                self.print_in_color(f'Wrong url: {self.url}', True)
+                printer.print_in_color(f'Wrong url: {url}', True)
                 exit()
             except ClientConnectorError:
-                self.print_in_color(
-                    f'Cannot connect to host {self.url}.[nodename nor servname provided, or not known]', True)
+                printer.print_in_color(
+                    f'Cannot connect to host {url}.[nodename nor servname provided, or not known]', True)
                 exit()
+
+    @staticmethod
+    async def download_jwt_secrets() -> List[str]:
+        """
+        tries to download list of jwt secrets from GitHub repo
+        `https://github.com/wallarm/jwt-secrets`
+        :return: List[str] list with jwt secrets line by line
+        """
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(JWT_SECRETS_FILE_URL) as response:
+                    if response.status == SUCCESS_RESPONSE_CODE:
+                        content = await response.text()
+                        return content.splitlines()
+                    else:
+                        raise Exception
+            except (ClientConnectorError, Exception):
+                return ReadWriteDocuments.read_jwt_secrets_file()
+
+
+class ReadWriteDocuments:
+
+    def __init__(self, result_file: str):
+        self.result_file: str = result_file
+
+    def save_result_to_file(self, result: List[Dict[str, str]]) -> None:
+        with open(self.result_file, 'w') as file:
+            file.write(str(json.dumps(result)))
+
+    @staticmethod
+    def read_jwt_secrets_file() -> List[str]:
+        with open(JWT_SECRETS_FILE_NAME, 'r') as file:
+            return file.readlines()
+
+
+class CookiesParser:
+
+    def __init__(self, cookies: Dict[str, str]):
+        self._cookies: Dict[str, str] = cookies
+        self._jwt_secrets: List[str] = []
 
     @property
     def cookies(self) -> Dict:
@@ -145,40 +149,6 @@ class LeakedCookie:
         self._cookies = cookies
 
     @property
-    def result(self) -> List[Dict[str, str]]:
-        """
-        list of found unsecure cookies
-        :return: List[Dict[str, str]]
-        """
-        return self._result
-
-    @result.setter
-    def result(self, result: Dict[str, str]) -> None:
-        """
-        setter for result
-        :param result: Dict[str, str]
-        :return: None
-        """
-        self._result.append(result)
-
-    @property
-    def result_file(self) -> str:
-        """
-        file name for saving result in json format
-        :return: str default result.json
-        """
-        return self._result_file
-
-    @result_file.setter
-    def result_file(self, file_name: str) -> None:
-        """
-        setter for result file name
-        :param file_name: str
-        :return: None
-        """
-        self._result_file = file_name
-
-    @property
     def jwt_secrets(self) -> List[str]:
         """
         List of public-available JWT secrets from google search and GitHub dorks
@@ -195,98 +165,112 @@ class LeakedCookie:
         """
         self._jwt_secrets = jwt_secrets
 
-    def print_cookies(self) -> None:
-        """
-        prints cookies to stdout
-        :return: None
-        """
-        self.print_in_color(str(self.cookies.items()))
-
-    def print_result(self) -> None:
-        """
-        prints result to stdout
-        :return: None
-        """
-        if self.result:
-            self.print_in_color(f'Cookies found:', True)
-        else:
-            self.print_in_color(f'Success, no jwt secrets found')
-        for value in self.result:
-            for key, cookie in value.items():
-                self.print_in_color(f'{key}: {cookie}', True)
-
-    async def download_jwt_secrets(self) -> None:
-        """
-        tries to download list of jwt secrets from GitHub repo
-        `https://github.com/wallarm/jwt-secrets`
-        :return: None
-        """
-        async with aiohttp.ClientSession() as session:
-            try:
-                async with session.get(JWT_SECRETS_FILE_URL) as response:
-                    if response.status == SUCCESS_RESPONSE_CODE:
-                        content = await response.text()
-                        self.jwt_secrets = content.splitlines()
-                    else:
-                        raise Exception
-            except (ClientConnectorError, Exception):
-                with open(JWT_SECRETS_FILE_NAME, 'r') as file:
-                    self.jwt_secrets = file.readlines()
-
-    async def parse_cookies(self) -> None:
+    async def parse_cookies(self) -> List[Dict[str, str]]:
         """
         downloads file with jwt secrets then
         loops thought it to find jwt secrets in cookies
-        :return: None
+        :return: List[Dict[str, str]]
         """
-        await self.download_jwt_secrets()
+        result = []
+        self._jwt_secrets = await Requests.download_jwt_secrets()
 
         for key, cookie in self.cookies.items():
             if cookie in self.jwt_secrets:
-                self.result = {'title': key, 'cookie': cookie}
+                result.append({'title': key, 'value': cookie})
+        return result
 
-    def save_result_to_file(self):
-        with open(self.result_file, 'w') as file:
-            file.write(str(json.dumps(self.result)))
 
-    def print_in_color(self, text: str, danger: bool = False) -> None:
+@attrs
+class LeakedCookie:
+    config: RunConfig = attrib()
+    _result: List[Dict[str, str]] = attrib(default=Factory(dict))
+
+    @classmethod
+    async def from_config(cls, config: RunConfig) -> 'LeakedCookie':
+        return cls(config=config)
+
+    async def run(self) -> None:
         """
-        prints output in color or without
-        depends on argument `--no-color`
-        green for success results
-        red if jwt secret found
-        :param text: str text to print
-        :param danger: bool print in green or red color
+        starts async jobs from the main script file
         :return: None
         """
-        if self.print_default:
-            print(text)
-        else:
-            if danger:
-                print(f'{Fore.RED}{text}')
-            else:
-                print(f'{Fore.GREEN}{text}')
+
+        # create pretty printer
+        printer = PrettyPrint(self.config.no_color)
+
+        # makes request to receive cookies
+        cookies = await Requests.make_request_to_target(URL(self.config.url), printer)
+        cookies.update({'SecretKeyToGenJWTs': 'secretissecret'})
+
+        # create cookie parser
+        cookie_parser = CookiesParser(cookies)
+        # print if needs found cookies from the target
+        if self.config.print_cookies and cookies:
+            printer.print_cookies(cookies)
+
+        # parsing cookies
+        self.result = await cookie_parser.parse_cookies()
+
+        # if not quiet print result to stdout
+        if not self.config.quiet:
+            printer.print_result(self.result)
+
+        # create write to file class
+        file_handler = ReadWriteDocuments(self.config.output)
+        # saving results to a file
+        file_handler.save_result_to_file(self.result)
 
     @property
-    def print_default(self) -> bool:
+    def result(self) -> List[Dict[str, str]]:
         """
-        propriety to print in color or not
-        :return: bool
+        list of found unsecure cookies
+        :return: List[Dict[str, str]]
         """
-        return self._print_default
+        return self._result
 
-    @print_default.setter
-    def print_default(self, default: bool) -> None:
+    @result.setter
+    def result(self, result: List[Dict[str, str]]) -> None:
         """
-        setter for print in color ot not
-        :param default: bool
+        setter for result
+        :param result: Dict[str, str]
         :return: None
         """
-        self._print_default = default
+        self._result = result
+
+
+def define_config_from_cmd(parsed_args: 'argparse.Namespace') -> RunConfig:
+    return RunConfig(
+        url=parsed_args.url,
+        output=parsed_args.output,
+        quiet=parsed_args.quiet,
+        print_cookies=parsed_args.print_cookies,
+        no_color=parsed_args.no_color
+    )
+
+
+def cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Finds jwt secrets in cookies')
+    # Add the arguments to the parser
+    parser.add_argument('-u', '--url', required=True, help='url', type=str)
+
+    parser.add_argument(
+        '-o', '--output', required=False, help='file to save result in json', default='result.json', type=str)
+
+    parser.add_argument(
+        '-q', '--quiet', required=False, help='quiet mod, only save to file', action='store_true', default=False)
+    parser.add_argument('-p', '--print-cookies', required=False, action='store_true', default=False,
+                        help='print to stdout found cookie list')
+    parser.add_argument('-n', '--no-color', required=False, action='store_true', default=False,
+                        help='print without colors to stdout')
+
+    return parser.parse_args()
 
 
 async def main():
-    await LeakedCookie.run()
+    parsed_args = cli()
+    run_config = define_config_from_cmd(parsed_args=parsed_args)
+    parser = await LeakedCookie.from_config(config=run_config)
+    await parser.run()
 
 
 if __name__ == '__main__':
